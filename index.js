16f2e7bb8810ba979bfbb249f43cf406f4f1e143
Frankie <frankiejosefej@gmail.com>
2023-02-13 01:32:58 -0500

    Solved all tasks

--- index.js
+++ index.js
@@ -15,10 +15,25 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+this.name = name;
+this.age = age;
+this.stomach = [];
 }
 
+Person.prototype.eat = function(food) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(food);
+  }
+};
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function() {
+return `${this.name}, ${this.age};`
+};
 
 /*
   TASK 2
@@ -36,9 +51,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
 
 
 /*
@@ -49,18 +71,24 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+Person.call(this, name, age, favoriteToy);
+this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}, ${this.favoriteToy} being the favorite toy`;
+};
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window binding is when the 'this' keyword pulls something from the global scope
+  2. Implicit binding is when a function is called as a method of an object, the 'this' value is set to the object the method is called on
+  3. Explicit binding is when you explicitly set the value of 'this' using 'call', 'apply', and 'bind'
+  4. New binding is when a function is called using the 'new' operator. A new object is made and the 'this' value to set to that new object
 */
 
 ///////// END OF CHALLENGE /////////
